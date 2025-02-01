d9f5a35bba4a1e635e3647acce7b84b24e415c76
Jose Augusto <joseaugustofarsousa@gmail.com>
2025-02-01 17:21:32 -0300

    API conectada com banco de dados.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import { pool } from './database';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+(async () => {
+    try {
+        const client = await pool.connect();
+        console.log('Database connected');
+        client.release();
+    } catch (err) {
+        console.error('Database connection error', err);
+    }
+})();
 
 app.listen(3000, () => console.log('Server running on port 3000'));
