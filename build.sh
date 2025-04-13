d6de830cff932cdebf841229d1734054269f2b4d
MarcusVynicius1 <91838315+MarcusVynicius1@users.noreply.github.com>
2025-04-13 15:28:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="432008002022.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 432008002022.dkr.ecr.us-east-1.amazonaws.com
-docker build -t aws_aula.
+docker build -t aws_aula .
 docker tag crud:latest $ECR_REGISTRY/crud:latest
 docker push crud:latest $ECR_REGISTRY/crud:latest
